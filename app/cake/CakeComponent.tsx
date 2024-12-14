85614f3664258766d04e0713ec83027b1e1df7a6
anastasiiagridchina <ns.gridchina@gmail.com>
2024-12-14 14:56:30 -0500

    feat: cake css

--- app/cake/CakeComponent.tsx
+++ app/cake/CakeComponent.tsx
@@ -1,18 +1,23 @@
 import React from "react";
-import styles from "./Cake.module.css"
+import styles from "./Cake.module.scss"
 import {CandleComponent} from "@/app/candle/CandleComponent";
 
 const CakeComponent = () => {
     const numberOfCandles = 1;
 
     const candlesArray: number[] = Array.from({length: numberOfCandles}, (_, index) => index);
-    // log
     return (
         <div className={styles.cake}>
-            <div className={styles.cakeBase}>
-                <div className={styles.candle}>
-                    {candlesArray.map((index) => <CandleComponent key={index}/>)}
-                </div>
+            <div className={styles.plate}></div>
+            <div className={styles.layer + " " + styles.layerTop}></div>
+            <div className={styles.layer + " " + styles.layerMiddle}></div>
+            <div className={styles.layer + " " + styles.layerBottom}></div>
+            <div className={styles.icing}></div>
+            <div className={styles.drip + " " + styles.drip1}></div>
+            <div className={styles.drip + " " + styles.drip2}></div>
+            <div className={styles.drip + " " + styles.drip3}></div>
+            <div className={styles.candle}>
+                {candlesArray.map((index) => <CandleComponent key={index}/>)}
             </div>
         </div>
     );
--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,6 @@
+import React from 'react'
+// import {FaStar, FaStarHalfAlt} from "react-icons/fa";
+// import {CiStar} from "react-icons/ci";
 import Head from "next/head";
 import CakeComponent from "@/app/cake/CakeComponent";
 
@@ -8,12 +11,17 @@ export default function Home() {
                 <title>Interactive Birthday Card</title>
                 <meta name="description" content="Blow out candles on a virtual birthday card!"/>
             </Head>
+            {/*<FaStar/>*/}
+            {/*<FaStarHalfAlt/>*/}
+            {/*<CiStar/>*/}
 
             <main className="main">
                 <h1 className="title">Happy Birthday!</h1>
                 <p>Blow out the candles below:</p>
                 <CakeComponent/>
             </main>
+
+
         </div>
     );
 }
