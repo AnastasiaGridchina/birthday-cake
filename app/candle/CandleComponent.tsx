74262e3dcba03d95f776dc59f519e76645740651
anastasiiagridchina <ns.gridchina@gmail.com>
2024-12-16 19:32:21 -0500

    feat: fixes

--- app/candle/CandleComponent.tsx
+++ app/candle/CandleComponent.tsx
@@ -14,39 +14,38 @@ type CandleComponentProps = {
 export const CandleComponent: React.FC<CandleComponentProps> = ({left, top, out, key}) => {
     const [isBlownOut, setIsBlownOut] = React.useState(false);
 
-    // const startListening = async () => {
-    //     try {
-    //         const stream = await navigator.mediaDevices.getUserMedia({audio: true});
-    //         const audioContext = new AudioContext();
-    //         const analyser = audioContext.createAnalyser();
-    //         const source = audioContext.createMediaStreamSource(stream);
-    //
-    //         source.connect(analyser);
-    //
-    //         const dataArray = new Uint8Array(analyser.frequencyBinCount);
-    //
-    //         function detectBlow() {
-    //             analyser.getByteFrequencyData(dataArray);
-    //             const volume =
-    //                 dataArray.reduce((sum, value) => sum + value, 0) / dataArray.length;
-    //
-    //             if (volume > 30) {
-    //                 // Trigger candle blowing out
-    //                 setIsBlownOut(true);
-    //             }
-    //         }
-    //
-    //         // Check for blowing sound at intervals
-    //         setInterval(detectBlow, 100);
-    //     } catch (err) {
-    //         console.error("Error accessing microphone:", err);
-    //     }
-    // };
-    
+    const startListening = async () => {
+        try {
+            const stream = await navigator.mediaDevices.getUserMedia({audio: true});
+            const audioContext = new AudioContext();
+            const analyser = audioContext.createAnalyser();
+            const source = audioContext.createMediaStreamSource(stream);
 
-    // const blowOutCandle = () => {
-    //     setIsBlownOut((prevState) => !prevState);
-    // }
+            source.connect(analyser);
+
+            const dataArray = new Uint8Array(analyser.frequencyBinCount);
+
+            function detectBlow() {
+                analyser.getByteFrequencyData(dataArray);
+                const volume =
+                    dataArray.reduce((sum, value) => sum + value, 0) / dataArray.length;
+
+                if (volume > 30) {
+                    // Trigger candle blowing out
+                    setIsBlownOut(true);
+                }
+            }
+
+            // Check for blowing sound at intervals
+            setInterval(detectBlow, 100);
+        } catch (err) {
+            console.error("Error accessing microphone:", err);
+        }
+    };
+
+    useEffect(() => {
+        startListening();
+    }, []);
 
 
     return (
