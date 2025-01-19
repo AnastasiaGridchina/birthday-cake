bced91019a351c034089d5705e7ae674b18dff75
anastasiiagridchina <ns.gridchina@gmail.com>
2025-01-18 19:01:14 -0500

    fix: fix

--- app/candle/CandleComponent.tsx
+++ app/candle/CandleComponent.tsx
@@ -1,5 +1,5 @@
 import styles from './Candle.module.css';
-import {AnimatePresence, motion} from "framer-motion";
+import {motion} from "framer-motion";
 
 import React from "react";
 import {CandlePositions} from "@/app/page";
